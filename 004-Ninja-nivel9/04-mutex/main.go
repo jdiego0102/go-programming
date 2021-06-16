201d9ef1d9022c2239d7b5087e5210ffe1508206
jdiego0102 <j.diego010297@gmail.com>
2021-06-16 12:00:56 -0500

    Ejericicio práctico Mutex (Reparar race condition).

---
+++ 004-Ninja-nivel9/04-mutex/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	increment := 0
+	gs := 100
+	wg.Add(gs)
+	var m sync.Mutex
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			m.Lock()
+			v := increment
+			v++
+			increment = v
+			fmt.Println(increment)
+			m.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("El valor final de incremento es:", increment)
+}
