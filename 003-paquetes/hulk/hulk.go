7d04cec3488517e0820b3b07d149833ef341d374
jdiego0102 <j.diego010297@gmail.com>
2021-06-17 13:54:56 -0500

    Documentación en Go.

--- 003-paquetes/hulk/hulk.go
+++ 003-paquetes/hulk/hulk.go
@@ -1,11 +1,14 @@
+// Package hulk del héroe de Marvel
 package hulk
 
 import "fmt"
 
+// Hulk saluda
 func Saludo() {
 	fmt.Println("Hola desde Hulk.")
 }
 
+// Saltar Hulk salta alto
 func Saltar() {
 	fmt.Println("Hulk Salta.")
 }
